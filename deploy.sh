6fa1771476ebc83ace087f6405a0e5e492f37228
concentricspheres <107502292+concentricspheres@users.noreply.github.com>
2023-03-09 20:04:22 -0600

    dont auto approve

--- deploy.sh
+++ deploy.sh
@@ -2,16 +2,16 @@
 set -e
 
 # Deploy only the VPC and VMS
-terraform apply -target=module.compute -target=google_compute_firewall.ssh -auto-approve
+terraform apply -target=module.compute -target=google_compute_firewall.ssh
 
 # Deploy the GKE Cluster.
-terraform apply -target=module.gke -auto-approve
+terraform apply -target=module.gke
 
 # Deploy the BackendConfig for health checks.
-terraform apply -target=kubernetes_manifest.vouch_backend_config --auto-approve
+terraform apply -target=kubernetes_manifest.vouch_backend_config
 
 # Kill the existing ssh session to the dirk1 proxy.
 kill $(ps aux | grep '[:]localhost:8888 -N -q -f' | awk '{print $2}')
 
 # Deploy everything else.
-terraform apply --auto-approve
+terraform apply
