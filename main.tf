383e8c5a1595acb29259af180ea3da4510d155d2
Yorick Downe <yorickdowne@gmail.com>
2023-03-31 12:06:55 -0400

    More CPU for Vouch

--- main.tf
+++ main.tf
@@ -222,13 +222,13 @@ resource "kubernetes_deployment" "vouch1" {
 
           resources {
             limits = {
-              cpu    = "0.25"
+              cpu    = "0.5"
               memory = "1.5Gi"
               ephemeral-storage = "10Mi"
             }
 
             requests = {
-              cpu    = "0.25"
+              cpu    = "0.5"
               memory = "1.5Gi"
               ephemeral-storage = "10Mi"
             }
