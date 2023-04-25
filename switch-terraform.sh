c20f4df19d1ccb545ce404839405bd2adc3c1a58
Yorick Downe <yorickdowne@gmail.com>
2023-04-25 08:29:55 -0400

    Smarter switch-terraform

--- switch-terraform.sh
+++ switch-terraform.sh
@@ -3,10 +3,17 @@
 # Kill old SSH sessions
 kill $(ps aux | grep '[:]localhost:8888 -N -q -f' | awk '{print $2}')
 
-# Glcoud
-echo "Choose the right project for this folder; don't set a default compute environ"
-gcloud init
-gcloud auth application-default login
+# GCloud
+if gcloud config configurations list | grep -q $(basename $(pwd)); then
+    echo "Activate the config named $(basename $(pwd))"
+    gcloud config configurations activate $(basename $(pwd))
+else
+    echo "Create a config named $(basename $(pwd)) with matching Google project; don't set a default compute region"
+    gcloud init
+fi
+if ! gcloud auth list | grep -q cryptomanufaktur.io; then
+    gcloud auth application-default login
+fi
 
 # Terraform
 terraform init -backend-config=backend.conf -reconfigure
