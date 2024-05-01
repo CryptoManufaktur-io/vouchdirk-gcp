d6d931af6500cff272759cef0f158a319555fd7b
Yorick Downe <yorickdowne@gmail.com>
2024-05-01 23:02:01 +0100

    Increase ephemeral to 50m for metrics

--- main.tf
+++ main.tf
@@ -986,13 +986,13 @@ resource "kubernetes_deployment" "kube_state_metrics" {
             limits = {
               cpu    = "0.25"
               memory = "0.5Gi"
-              ephemeral-storage = "10Mi"
+              ephemeral-storage = "50Mi"
             }
 
             requests = {
               cpu    = "0.25"
               memory = "0.5Gi"
-              ephemeral-storage = "10Mi"
+              ephemeral-storage = "50Mi"
             }
           }
         }
